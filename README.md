7b71a2ffef9fb978ec9febde7f92759753ecc46d
monofon <monofon@tramberend.de>
2019-02-17 11:38:13 +0100

    Upgrade dependencies

--- README.md
+++ README.md
@@ -1,17 +1,36 @@
-# Pandoc Format
+pandoc-format README
+====================
 
-A Visual Studio Code formatter for Markdown using [Pandoc](http://pandoc.org).
+This extensions uses [Pandoc] to format Markdown source code. It
+supports the standard *Format Document* and *Format Selection* actions.
 
-## Features
+Features
+--------
 
--   Supports document and document range formatting.
+The palette command *Pandoc: Format Document or Selection* is provided.
+If multiple formatters are registered for Markdown documents, this
+command can be used to invoke this specific formatter.
 
-## Requirements
+Requirements
+------------
 
-Requires [Pandoc](http://pandoc.org) to be installed.
+This extension requires at least [Pandoc] version 2.0 to be installed.
 
-## Release Notes
+Extension Settings
+------------------
 
-### 0.0.1
+This extension contributes the following settings:
 
-Initial release. Developed and tested on macOS.
+-   `pandoc-format.enable`: enable/disable this extension (default:
+    `true`)
+-   `pandoc-format.command`: set the path to the `pandoc` executable
+    including command line arguments (default: `pandoc`)
+
+Extension Command
+-----------------
+
+This extension contributes the following command:
+
+-   `pandoc-format.format`: Pandoc: Format Document or Selection
+
+  [Pandoc]: http://pandoc.org
--- src/extension.ts
+++ src/extension.ts
@@ -1,74 +1,144 @@
-'use strict';
+"use strict";
 
-import * as vscode from 'vscode';
-import * as child_process from 'child_process';
+import * as vscode from "vscode";
+import * as child_process from "child_process";
 
-class PandocFormatEditsProvider implements
-  vscode.DocumentFormattingEditProvider,
-  vscode.DocumentRangeFormattingEditProvider {
-  command: string;
-  arguments: Array<string>;
+class PandocFormatEditsProvider
+  implements
+    vscode.DocumentFormattingEditProvider,
+    vscode.DocumentRangeFormattingEditProvider {
+  pandocAvailable: boolean = false;
+  enable: boolean = true;
+  command: string = "pandoc";
+  arguments: Array<string> = [];
 
   constructor() {
-    const config = vscode.workspace.getConfiguration('pandocFormat');
-    const commandline = config.get('command', 'pandoc');
-    const args = commandline.split(' ');
+    this.configure();
+  }
 
+  configure() {
+    const config = vscode.workspace.getConfiguration("pandoc-format");
+    let args = config.get("command", "pandoc").split(" ");
+
+    this.enable = config.get("enable", true);
     this.command = args[0];
     this.arguments = args.slice(1);
 
-    console.log(args);
+    if (this.enable) {
+      let result = child_process.spawnSync(this.command, ["--version"], {
+        shell: true
+      });
+      if (!result.error) {
+        this.pandocAvailable = true;
+
+        console.log("pandoc-format: using executable: " + this.command);
+      } else {
+        this.pandocAvailable = false;
+        this.catchExecError(result.error);
+      }
+    }
   }
 
   formatPandoc(text: string) {
-    let result =
-      child_process.spawnSync(this.command, this.arguments, { 'input': text });
-    console.log("formatting ...");
+    let result = child_process.spawnSync(this.command, this.arguments, {
+      input: text,
+      shell: true
+    });
+
+    if (result.error) {
+      this.catchExecError(result.error);
+      return "";
+    }
+
     if (!result.status) {
-      console.log("... successfull.");
       return result.stdout.toString();
     } else {
-      console.log("... failed!");
-      vscode.window.showErrorMessage(result.stderr.toString());
-      return text;
+      vscode.window.showWarningMessage(result.stderr.toString().split("\n")[0]);
+      return "";
     }
   }
 
   provideDocumentFormattingEdits(
-    document: vscode.TextDocument, options: vscode.FormattingOptions,
-    token: vscode.CancellationToken):
-    vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
-    console.log("About to format:");
+    document: vscode.TextDocument,
+    options: vscode.FormattingOptions,
+    token: vscode.CancellationToken
+  ): vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
     let formatted = this.formatPandoc(document.getText());
-    if (formatted != '') {
-      console.log("formatting returned result.");
-      return [vscode.TextEdit.replace(
-        document.validateRange(new vscode.Range(0, 0, Infinity, Infinity)), formatted)];
-    }
-    else {
-      console.log("formatting result empty!");
+    if (formatted != "") {
+      let range = document.validateRange(
+        new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE)
+      );
+      return [vscode.TextEdit.replace(range, formatted)];
+    } else {
       return [];
     }
   }
 
   provideDocumentRangeFormattingEdits(
-    document: vscode.TextDocument, range: vscode.Range,
-    options: vscode.FormattingOptions, token: vscode.CancellationToken):
-    vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
+    document: vscode.TextDocument,
+    range: vscode.Range,
+    options: vscode.FormattingOptions,
+    token: vscode.CancellationToken
+  ): vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
     let formatted = this.formatPandoc(document.getText(range));
-    if (formatted != '')
+    if (formatted !== "") {
       return [vscode.TextEdit.replace(range, formatted)];
-    else
+    } else {
       return [];
+    }
+  }
+
+  catchExecError(error: NodeJS.ErrnoException) {
+    let message = error.message;
+    if (error.code === "ENOENT") {
+      message = `pandoc-format: Could not execute ${
+        this.command
+      }. Try specifying the pandoc-format.command setting?`;
+    }
+    vscode.window.showErrorMessage(message);
   }
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  // TODO Check for pandoc executable and version
-  vscode.languages.registerDocumentFormattingEditProvider(
-    'markdown', new PandocFormatEditsProvider);
-  vscode.languages.registerDocumentRangeFormattingEditProvider(
-    'markdown', new PandocFormatEditsProvider);
+  console.log("pandoc-format: active!");
+  let pandocFormatProvider = new PandocFormatEditsProvider();
+
+  if (pandocFormatProvider.pandocAvailable) {
+    vscode.languages.registerDocumentFormattingEditProvider(
+      "markdown",
+      pandocFormatProvider
+    );
+    console.log("pandoc-format: registered document formatting provider");
+
+    vscode.languages.registerDocumentRangeFormattingEditProvider(
+      "markdown",
+      pandocFormatProvider
+    );
+    console.log("pandoc-format: registered selection formatting provider");
+
+    vscode.commands.registerTextEditorCommand(
+      "pandoc-format.format",
+      (
+        textEditor: vscode.TextEditor,
+        edit: vscode.TextEditorEdit,
+        args: any[]
+      ) => {
+        let range = textEditor.selection.with();
+        if (range.isEmpty) {
+          range = textEditor.document.validateRange(
+            new vscode.Range(0, 0, Number.MAX_VALUE, Number.MAX_VALUE)
+          );
+        }
+        let formatted = pandocFormatProvider.formatPandoc(
+          textEditor.document.getText(range)
+        );
+        if (formatted !== "") {
+          edit.replace(range, formatted);
+        }
+      }
+    );
+    console.log("pandoc-format: registered palette command");
+  }
 }
 
-export function deactivate() { }
+export function deactivate() {}
